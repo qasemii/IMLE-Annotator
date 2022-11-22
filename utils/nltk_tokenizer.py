3d0cb5ed5d02d9ed39d03917fbb0f13d41727138
Reza Madani <rezamadani.ai@gmail.com>
2022-11-22 01:45:39 +0100

    Cleaning

---
+++ utils/nltk_tokenizer.py
@@ -0,0 +1,12 @@
+import nltk
+from nltk import word_tokenize
+from snippets.explore_data import get_data
+
+# nltk.download('punkt')
+
+data_path = '../data/esnli_train_1.csv'
+data = get_data(data_path)
+
+sample = data['sentences'][23]
+print(sample)
+print(word_tokenize(sample))
