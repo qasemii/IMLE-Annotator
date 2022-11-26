1a077f104462575732b4341c4ac0a0c407ca951d
Reza Madani <rezamadani.ai@gmail.com>
2022-11-26 16:14:51 +0100

    Change to Whole training data

--- esnli-cli.py
+++ esnli-cli.py
@@ -193,8 +193,8 @@ def main(argv):
     y_train_list = [label_to_id[label] for label in train_data['label']]
 
     # # select smaller dataset
-    X_train_list = X_train_list[:50000]
-    y_train_list = y_train_list[:50000]
+    # X_train_list = X_train_list[:50000]
+    # y_train_list = y_train_list[:50000]
 
     X_train = pad_sequences(X_train_list, max_len=maxlen)
     y_train = np.asarray(y_train_list)
