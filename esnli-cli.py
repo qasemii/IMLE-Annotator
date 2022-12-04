cf2d5fb43b123f0bc80f939bef8f36e0682113de
Reza Madani <rezamadani.ai@gmail.com>
2022-12-04 13:18:45 +0100

    Remove highlight loss

--- esnli-cli.py
+++ esnli-cli.py
@@ -161,7 +161,6 @@ def main(argv):
     tokenized_sentence = train_data['sentence']['merged']
 
     # the dictionary mapping words to their IDs
-    print('Preparing Token Lists ...')
     word_to_id = dict()
     token_id_counter = 3
     for token_list in tokenized_sentence:
@@ -430,18 +429,18 @@ def main(argv):
                 loss = loss_function(p, y)
 
                 # mask for machine selected tokens #############################################
-                selected_token_mask = model.z(x=X)[0]
-
-                # mask for human selected tokens
-                true_highlight_idx = train_data['highlight']['merged'][i]
-                true_token_mask = torch.zeros_like(selected_token_mask)
-                true_token_mask[true_highlight_idx] = 1
-
-                highlights_loss = highlight_loss_function(true_token_mask, selected_token_mask)
-                highlights_loss_value = highlights_loss.item()
-
-                if involve_highlights:
-                    loss = loss + highlights_loss
+                # selected_token_mask = model.z(x=X)[0]
+                #
+                # # mask for human selected tokens
+                # true_highlight_idx = train_data['highlight']['merged'][i]
+                # true_token_mask = torch.zeros_like(selected_token_mask)
+                # true_token_mask[true_highlight_idx] = 1
+                #
+                # highlights_loss = highlight_loss_function(true_token_mask, selected_token_mask)
+                # highlights_loss_value = highlights_loss.item()
+                #
+                # if involve_highlights:
+                #     loss = loss + highlights_loss
                 ##################################################################################
 
                 loss_value = loss.item()
@@ -449,7 +448,7 @@ def main(argv):
                     logger.info(f'Epoch {epoch_no}/{epochs}\tIteration {i + 1}\tLoss value: {loss_value:.4f}')
 
                 epoch_loss_values += [loss_value]
-                epoch_highlights_loss += [highlights_loss_value]
+                # epoch_highlights_loss += [highlights_loss_value]
 
                 loss.backward()
                 optimizer.step()
