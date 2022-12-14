d067ea6fbfe8a62e1afb3a1f3f8bdc8ac971c8b4
Reza Madani <rezamadani.ai@gmail.com>
2022-12-14 01:23:22 +0100

    Add scores to the output report for BeerAdvocate

--- l2x/torch/utils.py
+++ l2x/torch/utils.py
@@ -83,6 +83,7 @@ def subset_precision(model, aspect, id_to_word, word_to_id, select_k, device: to
             prediction = model.z(X_test_subset_t)
 
             predicted_score = model(X_test_subset_t)
+        ground_truth = data[anotr]['y'][aspect]
 
         x_val_selected = prediction[0].cpu().numpy() * X_test_subset
 
@@ -112,7 +113,7 @@ def subset_precision(model, aspect, id_to_word, word_to_id, select_k, device: to
             text_list[r[0]] = '\hlc[cyan!30]{' + text_list[r[0]]
             text_list[r[1] - 1] = text_list[r[1] - 1] + '}'
 
-        highlights.append(' '.join(text_list) + '\\\\')
+        highlights.append(' '.join(text_list) + f' predicted score: {predicted_score} - ground truth: {ground_truth}\\\\')
 
         # we make sure that we select at least 10 non-padding words
         # if we have more than select_k non-padding words selected, we allow it but count that in
