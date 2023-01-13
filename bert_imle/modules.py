d6d6f609c86236fdb9de4fb99e201939ae6a1182
Reza Madani <rezamadani.ai@gmail.com>
2023-01-13 02:01:22 +0100

    Update

--- bert_imle/modules.py
+++ bert_imle/modules.py
@@ -16,18 +16,6 @@ def init(layer: nn.Module):
         assert f'Do not know how to deal with {type(layer)}'
 
 
-def select_k(logits: Tensor, select_k: int) -> Tensor:
-    scores, indices = torch.topk(logits, select_k, sorted=True)
-    mask = torch.zeros_like(logits, device=logits.device).scatter_(-1, indices, 1.0)
-    return mask
-
-
-def mathias_select_k(logits: Tensor, select_k: int) -> Tensor:
-    scores, indices = torch.topk(logits, select_k, sorted=True)
-    thr_2d = scores[:, -1].view(-1, 1)
-    return (logits >= thr_2d).float()
-
-
 class Predictor(torch.nn.Module):
     def __init__(self, embedding_weights: Tensor,
                  hidden_dims: int,
