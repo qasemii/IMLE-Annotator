adb26abd91e9b4461193a9980f5e502244704818
Reza Madani <rezamadani.ai@gmail.com>
2023-01-17 15:04:33 +0100

    Model configuration completeed

--- bert_imle/modules.py
+++ bert_imle/modules.py
@@ -2,7 +2,10 @@ import torch
 import math
 
 from torch import nn, Tensor
-from typing import Optional, Tuple, Callable
+from typing import Optional, Tuple, Callable, Any
+
+from transformers import (
+    AutoModelForTokenClassification)
 
 
 # Weight initializer
@@ -12,6 +15,7 @@ def init(layer: nn.Module):
         torch.nn.init.zeros_(layer.bias)
     else:
         assert f'Do not know how to deal with {type(layer)}'
+
 
 # Differentiable select-k model
 class DifferentiableSelectKModel(torch.nn.Module):
@@ -28,17 +32,16 @@ class DifferentiableSelectKModel(torch.nn.Module):
 
 # prediction model
 class Predictor(torch.nn.Module):
-    def __init__(self, embedding_weights: Tensor,
+    def __init__(self,
+                 embedding_dims,
                  hidden_dims: int,
                  select_k: int):
         super().__init__()
-        self.nb_words, self.embedding_dim = embedding_weights.shape
+        self.embedding_dims = embedding_dims
         self.hidden_dims = hidden_dims
         self.select_k = float(select_k)
 
-        self.embeddings = nn.Embedding.from_pretrained(embedding_weights, freeze=True)
-
-        self.layer_1 = nn.Linear(in_features=self.embedding_dim, out_features=self.hidden_dims, bias=True)
+        self.layer_1 = nn.Linear(in_features=self.embedding_dims, out_features=self.hidden_dims, bias=True)
         init(self.layer_1)
 
         self.layer_2 = nn.Linear(in_features=self.hidden_dims, out_features=1, bias=True)
@@ -47,13 +50,62 @@ class Predictor(torch.nn.Module):
         self.activation = nn.ReLU()
         self.output_activation = nn.Sigmoid()
 
-    def forward(self, x: Tensor,
-                mask: Tensor) -> Tensor:
-        x_emb = self.embeddings(x)  # [B, T] -> [B, T, E]
-        res = x_emb * mask  # [B, S, E]
-        res = torch.sum(res, dim=1) / self.select_k  # [B, E]
+    def forward(self, embedding_weights: Tensor) -> Tensor:
+        # x_emb = self.embeddings(x)  # [B, T] -> [B, T, E]
+        # res = x_emb * mask  # [B, S, E]
+
+        res = torch.sum(embedding_weights, dim=1) / self.select_k  # [B, E]
         res = self.layer_1(res)  # [B, H]
         res = self.activation(res)  # [B, 1]
         res = self.layer_2(res)
         res = self.output_activation(res)
         return res
+
+
+# Main model (BERT + Select_K + Predictor)
+class Model(torch.nn.Module):
+    def __init__(self,
+                 model_name_or_path,
+                 config,
+                 hidden_dims: int,
+                 select_k: int,
+                 differentiable_select_k: Optional[Callable[[Tensor], Tensor]] = None):
+        super().__init__()
+
+        # BERT model
+        self.bert_model = AutoModelForTokenClassification.from_pretrained(
+            pretrained_model_name_or_path=model_name_or_path,
+            from_tf=bool(".ckpt" in model_name_or_path),
+            config=config)
+
+        # Differentiable select_k
+        self.differentiable_select_k = differentiable_select_k
+
+        # FC Predictor
+        self.predictor = Predictor(
+            embedding_dims=768,
+            hidden_dims=hidden_dims,
+            select_k=select_k)
+
+    def z(self, **kwargs) -> tuple[Tensor, Any]:
+        # prediction includes score for labels in ['O', 'B-Xxx', 'I-Xxx']
+        bert_output = self.bert_model(**kwargs)
+        predictions = bert_output.logits
+        # [B, T]
+        token_scores = torch.nn.Softmax(dim=2)(predictions)[:, :, 1]
+        token_masks = self.differentiable_select_k(token_scores)
+        # [B, T, 1]
+        token_masks = token_masks.unsqueeze(dim=-1)
+
+        # (for now) we consider BERT last layer embeddings as word embeddings
+        seq_embeddings = bert_output.hidden_states[0]
+        embedding_weights = seq_embeddings * token_masks
+        return token_masks, embedding_weights
+
+    def forward(self, **kwargs) -> Tensor:
+        # [B, T]
+        _, embedding_weights = self.z(**kwargs)
+
+        p = self.predictor(embedding_weights)
+
+        return p
