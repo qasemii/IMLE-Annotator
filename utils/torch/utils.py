27f0ae288cb75957d85a8201f9baee7dddaeae07
Reza Madani <rezamadani.ai@gmail.com>
2023-05-14 21:04:57 +0100

    Update

--- utils/torch/utils.py
+++ utils/torch/utils.py
@@ -42,7 +42,7 @@ def plot_stats(data, title='Plot'):
 def subset_precision(model, aspect, id_to_word, word_to_id, select_k, device: torch.device, max_len: int = 350):
     data = []
     num_annotated_reviews = 0
-    with open("imle-annotator/data/BeerAdvocate/annotations.json") as fin:
+    with open("data/BeerAdvocate/annotations.json") as fin:
         for line in fin:
             item = json.loads(line)
             data.append(item)
